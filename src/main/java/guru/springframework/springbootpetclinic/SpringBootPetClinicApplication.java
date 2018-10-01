5a7ffbd2e721b4f3e1153b8a0a57930f787771cd
naftab <naumanaftab@gmail.com>
2018-10-01 22:30:08 +0200

    renamed boot class.

--- src/main/java/guru/springframework/springbootpetclinic/SpringBootPetClinicApplication.java
+++ src/main/java/guru/springframework/springbootpetclinic/SpringBootPetClinicApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPetclinicApplication {
+public class SpringBootPetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootPetclinicApplication.class, args);
+        SpringApplication.run(SpringBootPetClinicApplication.class, args);
     }
 }
--- src/test/java/guru/springframework/springbootpetclinic/SpringBootPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/springbootpetclinic/SpringBootPetClinicApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootPetclinicApplicationTests {
+public class SpringBootPetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
