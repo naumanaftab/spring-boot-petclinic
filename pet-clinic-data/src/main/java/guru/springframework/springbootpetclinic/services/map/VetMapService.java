48126a06a7ed362fc8e0c0baa6c6051ccadfd436
naftab <naumanaftab@gmail.com>
2018-10-03 16:04:33 +0200

    refacoted to have map service for persistence.  Closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/springbootpetclinic/services/map/VetMapService.java
@@ -0,0 +1,8 @@
+package guru.springframework.springbootpetclinic.services.map;
+
+import guru.springframework.springbootpetclinic.model.Vet;
+import guru.springframework.springbootpetclinic.services.VetService;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+}
