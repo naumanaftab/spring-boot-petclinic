6c6c0b546b957648d87a90e7d845dbc24bc21b81
naftab <naumanaftab@gmail.com>
2018-10-07 20:05:49 +0200

    Added Speciality Map Service. Closes #32

---
+++ pet-clinic-data/src/main/java/guru/springframework/springbootpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package guru.springframework.springbootpetclinic.services;
+
+import guru.springframework.springbootpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/springbootpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,7 @@
+package guru.springframework.springbootpetclinic.services.map;
+
+import guru.springframework.springbootpetclinic.model.Speciality;
+import guru.springframework.springbootpetclinic.services.SpecialityService;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+}
