7eb4e9e7935eb65dba9012388ddb577268873105
naftab <naumanaftab@gmail.com>
2018-11-19 23:16:17 +0100

    Looks like a final fix for CI with CV. #89

--- pet-clinic-web/src/main/java/master/springframework/springbootpetclinic/SpringBootPetClinicApplication.java
+++ pet-clinic-web/src/main/java/master/springframework/springbootpetclinic/SpringBootPetClinicApplication.java
@@ -1,25 +1,24 @@
 package master.springframework.springbootpetclinic;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringBootPetClinicApplication extends SpringBootServletInitializer {
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return configureApplication(builder);
-//    }
-//
-//    public static void main(String[] args) {
-//        configureApplication(new SpringApplicationBuilder()).run(args);
-//    }
-//
-//    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
-//        return builder.sources(SpringBootPetClinicApplication.class).bannerMode(Banner.Mode.OFF);
-//    }
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootPetClinicApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return configureApplication(builder);
     }
+
+    public static void main(String[] args) {
+        configureApplication(new SpringApplicationBuilder()).run(args);
+    }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootPetClinicApplication.class).bannerMode(Banner.Mode.OFF);
+    }
+
 }
