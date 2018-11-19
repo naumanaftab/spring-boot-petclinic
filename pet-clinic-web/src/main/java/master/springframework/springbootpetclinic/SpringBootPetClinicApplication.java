140ea43f7df82079f6f420da6b6741cac8073bed
naftab <naumanaftab@gmail.com>
2018-11-19 23:07:15 +0100

    Reverting The Change of Spring Boot Application setup to work in both exploaded and compressed mde. #89

--- pet-clinic-web/src/main/java/master/springframework/springbootpetclinic/SpringBootPetClinicApplication.java
+++ pet-clinic-web/src/main/java/master/springframework/springbootpetclinic/SpringBootPetClinicApplication.java
@@ -1,24 +1,25 @@
 package master.springframework.springbootpetclinic;
 
-import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringBootPetClinicApplication extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return configureApplication(builder);
-    }
-
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return configureApplication(builder);
+//    }
+//
+//    public static void main(String[] args) {
+//        configureApplication(new SpringApplicationBuilder()).run(args);
+//    }
+//
+//    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+//        return builder.sources(SpringBootPetClinicApplication.class).bannerMode(Banner.Mode.OFF);
+//    }
     public static void main(String[] args) {
-        configureApplication(new SpringApplicationBuilder()).run(args);
+        SpringApplication.run(SpringBootPetClinicApplication.class, args);
     }
-
-    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
-        return builder.sources(SpringBootPetClinicApplication.class).bannerMode(Banner.Mode.OFF);
-    }
-
 }
